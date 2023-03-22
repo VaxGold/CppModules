77d53beb7fbb2c352ce65e233a97f5864e795bba
Oscar Mercade de lucas <omercade@c3r10s6.42madrid.com>
2023-03-22 21:11:42 +0100

    Rebuild ex00

--- CPPModule09/ex00/BitcoinExchange.cpp
+++ CPPModule09/ex00/BitcoinExchange.cpp
@@ -6,126 +6,120 @@
 /*   By: omercade <omercade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 18:40:32 by omercade          #+#    #+#             */
-/*   Updated: 2023/03/21 21:09:50 by omercade         ###   ########.fr       */
+/*   Updated: 2023/03/22 21:10:39 by omercade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-BitcoinExchange::BitcoinExchange()
-{
-    
-}
+BitcoinExchange::BitcoinExchange() { }
 
-BitcoinExchange::BitcoinExchange(int fd)
+BitcoinExchange::BitcoinExchange(FILE *file)
 {
-    
+    setData(file);
 }
 
 BitcoinExchange::BitcoinExchange(BitcoinExchange const &other)
 {
-    
+    *this = other;
 }
 
-BitcoinExchange::~BitcoinExchange()
+BitcoinExchange::~BitcoinExchange() { }
+
+void BitcoinExchange::setData(FILE *file)
 {
-    
+    //TODO
+    return ;
+}
+
+std::queue<std::string> BitcoinExchange::getData() const
+{
+    return (this->_data);
 }
 
 BitcoinExchange	&BitcoinExchange::operator=(BitcoinExchange const &other)
 {
-    
+    if (this != &other)
+    {
+        this->_data = other.getData();
+    }
+    return (*this);
 }
 
-int BitcoinExchange::validateDate(std::string date)
+int BitcoinExchange::validDate(std::string date)
 {
     int day;
     int month;
     int year;
-
+    
+    //TODO RETURNS
     //YEAR
     if (year < 0)
-    {
-        throw BitcoinExchange::DateException();
-        return (1);
-    }
+        return (0);
     
     //MONTH
     if (month < 1 || month > 12)
-    {
-        throw BitcoinExchange::DateException();
-        return (1);
-    }
+        return (0);
 
     //DAY
     if (day < 1 || day > 31)
-    {
-        throw BitcoinExchange::DateException();
-        return (1);
-    }
+        return (0);
     if (month == 2)
     {
         if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0))
         {
             if (day > 29)
-            {
-                throw BitcoinExchange::DateException();
-                return (1);
-            }
+                return (0);
         }
         else
         {
             if (day > 28)
-            {
-                throw BitcoinExchange::DateException();
-                return (1);
-            }
+                return (0);
         }
     }
     else if ((month <= 7 && month % 2 == 0) || (month >= 8 && month % 2 == 1))
     {
         if (day > 30)
-        {
-            throw BitcoinExchange::DateException();
-            return (1);
-        }
+            return (0);
     }
-    return (0);
+    return (1);
 }
 
-int BitcoinExchange::validateValue(int value)
+int BitcoinExchange::validValue(std::string value)
 {
-    if (value < 0)
-        throw BitcoinExchange::NegativeValueException();
-    else if (value > 1000)
-        throw BitcoinExchange::RangeValueException();
-    else
+    float n;
+
+    n = atof(value.c_str());
+    if (n < 0)
+        return (-1);
+    else if (n > 1000)
         return (0);
     return (1);
 }
 
-int BitcoinExchange::validateValue(float value)
-{
-    if (value < 0)
-        throw BitcoinExchange::NegativeValueException();
-    else if (value > 1000)
-        throw BitcoinExchange::RangeValueException();
-    else
-        return (0);
-    return (1);
-}
-
-const char* BitcoinExchange::DateException::what() const throw()
+std::string DateError (std::string date)
 {
 	return "Error: bad input => ";
 }
 
-const char* BitcoinExchange::RangeValueException::what() const throw()
+std::string ValueRangeError ()
 {
-	return "Error: too large a number.";
+	return "Error: too large number.";
 }
 
-const char* BitcoinExchange::NegativeValueException::what() const throw()
+std::string NegativeValueError ()
 {
 	return "Error: not a positive number.";
+}
+
+std::ostream& operator << (std::ostream &out, const BitcoinExchange &bte)
+{
+    std::queue<std::string> tmp = bte.getData();
+
+    while (!tmp.empty())
+    {
+        out << tmp.front() << std::endl;
+        tmp.pop();
+    }
+	return out;
 }
--- CPPModule09/ex00/BitcoinExchange.hpp
+++ CPPModule09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: omercade <omercade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 18:40:41 by omercade          #+#    #+#             */
-/*   Updated: 2023/03/21 21:23:30 by omercade         ###   ########.fr       */
+/*   Updated: 2023/03/22 21:08:21 by omercade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,60 @@
 # define BITCOINEXCHANGE_HPP
 
 # include <iostream>
+# include <cstdio>
+# include <cstdlib>
+# include <queue>
+
+// struct bitdata
+// {
+// 	int dd;
+// 	int mm;
+// 	int yy;
+// 	float nbc;
+// 	float value;
+// };
+
 
 class	BitcoinExchange
 {
 	private:
-			
-
+			std::queue<std::string> _data;
 	public:
 			BitcoinExchange();
 			BitcoinExchange(FILE *file);
 			BitcoinExchange(BitcoinExchange const &other);
 			virtual ~BitcoinExchange();
 
+			void setData(FILE *file);
+			std::queue<std::string> getData() const;
+
+            int validDate(std::string date);
+            int validValue(std::string value);
+			
 			BitcoinExchange	&operator=(BitcoinExchange const &other);
 
-            int validateDate(std::string date);
-            int validateValue(int value);
-            int validateValue(float value);
+			std::string DateError (std::string date);
+			std::string ValueRangeError ();
+			std::string NegativeValueError ();
             
-			class DateException : public std::exception
-			{
-				public:
-					virtual const char *what() const throw ();
-			};
+			//Excepciones
+			// class DateException : public std::exception
+			// {
+			// 	public:
+			// 		virtual const char *what() const throw ();
+			// };
 
-            class RangeValueException : public std::exception
-			{
-				public:
-					virtual const char *what() const throw ();
-			};
+            // class RangeValueException : public std::exception
+			// {
+			// 	public:
+			// 		virtual const char *what() const throw ();
+			// };
 
-            class NegativeValueException : public std::exception
-			{
-				public:
-					virtual const char *what() const throw ();
-			};
+            // class NegativeValueException : public std::exception
+			// {
+			// 	public:
+			// 		virtual const char *what() const throw ();
+			// };
 };
 
 std::ostream& operator << ( std::ostream &out, const BitcoinExchange &bte);
